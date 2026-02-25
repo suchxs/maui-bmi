ad3200eb12452fc1f407f1ee0408e44a3aa947dc
suchxs <corsegajoseph@gmail.com>
2026-02-26 01:40:38 +0800

    REFACTOR: removed inner native border

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,11 @@
 ﻿using Microsoft.Extensions.Logging;
+#if ANDROID
+using Microsoft.Maui.Handlers;
+#endif
+#if IOS || MACCATALYST
+using Microsoft.Maui.Handlers;
+using UIKit;
+#endif
 
 namespace maui_bmi;
 
@@ -14,6 +21,38 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
+
+#if ANDROID
+        EntryHandler.Mapper.AppendToMapping("CleanInputChrome", (handler, view) =>
+        {
+            handler.PlatformView.Background = null;
+            handler.PlatformView.SetPadding(0, 0, 0, 0);
+        });
+
+        PickerHandler.Mapper.AppendToMapping("CleanInputChrome", (handler, view) =>
+        {
+            handler.PlatformView.Background = null;
+            handler.PlatformView.SetPadding(0, 0, 0, 0);
+        });
+#endif
+
+#if IOS || MACCATALYST
+        EntryHandler.Mapper.AppendToMapping("CleanInputChrome", (handler, view) =>
+        {
+            handler.PlatformView.BorderStyle = UITextBorderStyle.None;
+            handler.PlatformView.BackgroundColor = UIColor.Clear;
+            handler.PlatformView.Layer.BorderWidth = 0;
+            handler.PlatformView.Layer.ShadowOpacity = 0;
+        });
+
+        PickerHandler.Mapper.AppendToMapping("CleanInputChrome", (handler, view) =>
+        {
+            handler.PlatformView.BorderStyle = UITextBorderStyle.None;
+            handler.PlatformView.BackgroundColor = UIColor.Clear;
+            handler.PlatformView.Layer.BorderWidth = 0;
+            handler.PlatformView.Layer.ShadowOpacity = 0;
+        });
+#endif
 
 #if DEBUG
         builder.Logging.AddDebug();
